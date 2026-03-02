512aca975cf5bdbc2bce8734ebd88cfc892a40b3
Vadivelu M <vadivelumsolo@gmail.com>
2026-03-03 00:26:02 +0530

    everything added newly

---
+++ mul.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+a=10
+b=20
+
+c=$((a*b))
+echo "mul of $a and $b is $c"
+
+echo " done "
---
+++ sub.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+a=10
+b=20
+
+c=$((a-b))
+echo "substraction of $a and $b is $c"
+echo " done "
